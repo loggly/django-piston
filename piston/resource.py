67d4c74dad7669e34842e3641adb9104c0997ed5
♥ Ivan Tam ♥ <ivan@loggly.com>
2012-07-31 15:50:07 +0000

    Fix JSON emitter switch
    /api/private/inputs was not returning in a sane JSON format

--- piston/resource.py
+++ piston/resource.py
@@ -218,7 +218,7 @@ class Resource(object):
         if not isinstance(result, HttpResponse):
             return False
         elif django.VERSION >= (1, 4):
-            return not result._base_content_is_iter
+            return result._base_content_is_iter
         else:
             return result._is_string
 
